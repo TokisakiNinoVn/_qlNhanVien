9ab04582dd9cb79ddfbe512232ddd7eaa803d714
Nino <110331442+tan-nino-23@users.noreply.github.com>
2022-12-12 17:28:19 +0700

    Update TestNV.java

--- TestNV.java
+++ TestNV.java
@@ -5,26 +5,12 @@ import java.util.*;
 public class TestNV {
 
 	public static void main(String[] args) {
-//		NhanVien s1 = new NhanVien();
-//		s1.nhapNV();	
-//		NhanVien s2 = new NhanVien();
-//		s2.nhapNV();
-//		
-//		s1.xuatNV();		
-//		s2.xuatNV();
-//		
-//		System.out.printf("\nTong so nhan vien la: %d", Nguoi.SumDS);
-//		NhanVien[] nv = new NhanVien[100];
-//			Scanner Huahua = new Scanner(System.in);
-//			int soNV = 2;
-//			int a = nv.length;
-//			System.out.printf("\n[%d]\n", a);
-//			
-//			for(int i = 0; i < soNV; i++) {
-//				System.out.printf("Nhap vao thong tin nhan vien thu %d: ", i+1);
-//				nv[i].nhapNV(); // ????????????????????????
-//			
-//		}
+		NhanVien s1 = new NhanVien();
+		s1.nhapNV(s1);	
+		NhanVien s2 = new NhanVien();
+		s2.nhapNV(s2);
+		
+		s1.xuatNV();		
+		s2.xuatNV();	
 	}
-
 }
